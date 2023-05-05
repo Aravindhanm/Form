83d3fe68beea63f9c8ab5889d244c6d1545a2cb4
Aravindhanm <105432938+Aravindhanm@users.noreply.github.com>
2023-05-05 12:35:11 +0530

    Add files via upload

---
+++ Form/database.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "postgres";
+$username = "postgres";
+$password = "postgres";
+$database = "postgres";
+try{
+
+    $conn = new PDO("pgsql:host=$servername;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e){
+    echo "connection failed". $e-> getMessage();
+}
+?>
---
+++ Form/formadd.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div class="crd">
+            <div class="head">
+                <h3>Inserted List <a href="form.php">Back</a></h3>
+            </div>
+            <form action="code.php" method="post">
+           
+            <label>Name</label>
+        <input type="text" name="ename">
+        <br>
+        <label>Email</label>
+        <input type="text" name="email">
+        <br>
+        <label>Date Of Birth</label>
+        <input type="date" name="dob">
+        <br>
+        <label>Mobile</label>
+        <input type="text" name="mob">
+        <br>
+        <label>Comment</label>
+        <textarea rows="5" cols="40"  name="comment"></textarea>
+        <br>
+        
+        <button type="submit" name="save">Save</button>
+
+            </form>
+        </div>
+    </body>
+</html>
