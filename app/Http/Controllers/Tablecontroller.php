c0a05af6412d29e9a2fdd86d9c2a0bef8286b5c1
Aravindhanm <aravindhan674@gmail.com>
2023-05-22 10:32:07 +0530

    hellobladephp

--- app/Http/Controllers/Tablecontroller.php
+++ app/Http/Controllers/Tablecontroller.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Validator;
 
 class TableController extends Controller
 {
+  
     /**
      * Display a listing of the resource.
      */
@@ -19,21 +20,20 @@ class TableController extends Controller
     $search = $request->input('search');
     $sort = $request->input('sort', 'id');
     $sortOrder = $request->input('sort_order', 'asc');
-
     $query = Table::query();
-
     if ($search) {
         $query->where('name', 'like', "%$search%")
             ->orWhere('email', 'like', "%$search%")
             ->orWhere('dob', 'like', "%$search%")
             ->orWhere('mob', 'like', "%$search%");
     }
-
     $tables = $query->orderBy($sort, $sortOrder)->paginate(10);
     $tables->appends(['search' => $search, 'sort' => $sort, 'sort_order' => $sortOrder]);
-
     return view('tables.index', compact('tables', 'sort', 'sortOrder', 'search'));
-} /**
+}
+
+
+ /**
      * Show the form for creating a new resource.
      */
     public function create(): View
@@ -41,12 +41,13 @@ class TableController extends Controller
         return view('tables.create');
     }
 
+
+
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request): RedirectResponse
 {
-
     $request->validate([
         'name' => 'required|string|max:100',
         'email' => 'required|email|unique:_table|max:100',
@@ -69,13 +70,12 @@ class TableController extends Controller
         $image->storeAs('public/images', $imageName);
         $input['image'] = $imageName;
     }
-
-
-
     Table::create($input);
-
     return redirect('table')->with('success', 'List Added Successfully');
-}/**
+}
+
+
+/**
      * Show the form for editing the specified resource.
      */
     public function edit(string $id): View
@@ -84,6 +84,8 @@ class TableController extends Controller
             return view('tables.edit')->with('tables',$tables);
     }
 
+
+
     /**
      * Update the specified resource in storage.
      */
@@ -91,49 +93,37 @@ class TableController extends Controller
 {
     $table = Table::findOrFail($id);
     $input = $request->all();
-
     if ($request->hasFile('image')) {
         // Delete the previous image from storage if it exists
         if ($table->image) {
             Storage::disk('public')->delete('images/' . $table->image);
         }
-
         // Store the new image
         $image = $request->file('image');
         $imageName = time() . '_' . $image->getClientOriginalName();
         $image->storeAs('public/images', $imageName);
         $input['image'] = $imageName;
     }
-
     $table->update($input);
-
     return redirect('table')->with('success', 'List Updated Successfully');
-}
+}  
 
 
-
-
-
-
-
-    /**
+/**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id): RedirectResponse
     {
         $table = Table::findOrFail($id);
-
-        // Delete the image from the folder
+       // Delete the image from the folder
         if ($table->image) {
             $imagePath = storage_path('app/public/images/' . $table->image);
             if (file_exists($imagePath)) {
                 unlink($imagePath);
             }
         }
-
         // Delete the record from the database
         $table->delete();
-
         return redirect('table')->with('delete', 'List Deleted successfully');
     }
 }
